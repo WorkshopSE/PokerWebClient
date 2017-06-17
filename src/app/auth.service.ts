cb2d2fb5a8ddbc67c25859518536d6b6e8a3c472
idanizi <idanizi@gmail.com>
2017-06-18 00:36:10 +0300

    implementing login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,23 +1,44 @@
 import {Injectable} from '@angular/core';
+import {Http, Headers} from "@angular/http";
+import "rxjs/add/operator/toPromise";
+import {promise} from "selenium-webdriver";
 
 @Injectable()
 export class AuthService {
 
-  constructor() {
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(private http: Http, private url: string) {
+    this.url = this.GetServerUrl();
   }
 
-  // stub
-  Login(): Promise<any> {
+  // TODO: auto route to server on same domain
+  private GetServerUrl(): string {
+    return 'http://localhost:9000/api/';
+  }
+
+  // TODO test this
+  public Login(username: string, password: string): Promise<any> {
+    return this.http.post(this.url + 'login', JSON.stringify({
+        'username': username,
+        'password': password
+      }),
+      {headers: this.headers}
+    ).toPromise()
+      .then(response => response.json().data)
+      .catch(err => {
+        console.log('Login error at auth service', err);
+        return Promise.reject(err.message || err);
+      });
+  }
+
+  // TODO implement
+  public Logout(): Promise<any> {
     return null;
   }
 
-  // stub
-  Logout(): Promise<any> {
-    return null;
-  }
-
-  // stub
-  SignIn(): Promise<any> {
+  // TODO implement
+  public SignIn(): Promise<any> {
     return null;
   }
 
