e78cc886285688a446f420d16780596523ae634f
idanizi <idanizi@gmail.com>
2017-06-26 16:07:22 +0300

    working

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokerWebClientApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pokerWebClientApp
+ */
+angular.module('pokerWebClientApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pokerWebClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pokerWebClientApp
+ */
+angular.module('pokerWebClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
